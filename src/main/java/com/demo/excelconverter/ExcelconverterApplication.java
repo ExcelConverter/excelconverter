0835a0f5921c48e234fc5b8fd88a5428fdfbd0b8
magidragon <magidragon@foxmail.com>
2022-07-29 23:31:09 +0800

    [INIT]Project Init!

---
+++ src/main/java/com/demo/excelconverter/ExcelconverterApplication.java
@@ -0,0 +1,13 @@
+package com.demo.excelconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelconverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelconverterApplication.class, args);
+    }
+
+}
